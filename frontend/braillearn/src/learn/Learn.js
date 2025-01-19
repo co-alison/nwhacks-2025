202642d9db150fe8a3935f26f6fd8d639ecb127a
Cindy Cui <misscindycui@gmail.com>
2025-01-18 16:31:29 -0800

    updates to learn page

--- frontend/braillearn/src/learn/Learn.js
+++ frontend/braillearn/src/learn/Learn.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function Learn() {
-  return (
-    <div className="Learn">
-      <header className="Learn-header">
-        <h1>Learn Braille</h1>
-      </header>
-    </div>
-  );
+    const [char, setChar] = useState('0');
+
+    const getCharacterValue = () => {
+        const inputField = document.getElementById('char-input');
+        if (inputField) {
+            setChar(inputField.value);
+            // send selected char to api to show
+            const res = axios.get(
+                `http://localhost:3001/send-letter?letter=${char}`
+            );
+        }
+    };
+
+
+    return (
+        <div className='Learn'>
+            <header className='Learn-header'>
+                <h1>Learn Braille</h1>
+                <input id='char-input' type='text' maxLength='1' name='char' />
+                <button
+                    type='button'
+                    id='display-btn'
+                    onClick={getCharacterValue}
+                >
+                    Display
+                </button>
+            </header>
+        </div>
+    );
 }
 
 export default Learn;
--- frontend/braillearn/src/utils/constants.js
+++ frontend/braillearn/src/utils/constants.js
@@ -2,3 +2,8 @@ export const states = {
     display: "display",
     listen: "listen"
 };
+
+export const learnStates = {
+    textInput: "text",
+    speechInput: "speechInput"
+}
