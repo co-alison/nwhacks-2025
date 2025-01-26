18b071e8ef710ac2bea2163b5e85252ea6200301
Cindy Cui <misscindycui@gmail.com>
2025-01-26 15:56:28 -0800

    connect Learn pages to server

--- frontend/braillearn/src/pages/Modules/ModulePages.js
+++ frontend/braillearn/src/pages/Modules/ModulePages.js
@@ -1,21 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
 import { Box, Typography, ThemeProvider, Button } from '@mui/material';
 import theme from '../../styles/theme';
 import BackButton from '../../components/BackButton';
+import { sendChar } from '../../utils/serverApi';
 
 // Individual page type components
-const IntroductionPage = ({
-    module /*isCompleted, onComplete*/,
-    // charsCovered,
-    // charsReviewed,
-    // charDescription,
-}) => {
+const IntroductionPage = ({ module /*isCompleted, onComplete*/ }) => {
     const reviews = module.charsReviewed
         ? ` and reviews characters ${module.charsReviewed}`
         : '';
-    const moduleNumber = module.id[0];
     var description = `This module covers the characters ${module.charsCovered}${reviews}.`;
 
     return (
@@ -48,18 +42,58 @@ const IntroductionPage = ({
     );
 };
 
+// INVARIANT: expects module.char to be a string that contains two quotation marks " ",
+// between which contains the singular character to be displayed. I.e. for module.char = "lowercase "a""",
+// the character displayed will be the single character "a".
 const LearnPage = ({
     module /*isCompleted, onComplete*/,
     // char,
     // representation,
 }) => {
+    // TODO: do we want to repeatedly display and clear the same character?
+    // useEffect(() => {
+    //     let isCancelled = false; // To handle cleanup when the user exits this page
+
+    //     const displayAndClearChar = async () => {
+    //         if (isCancelled) return;
+
+    //         // Send the first character
+    //         await sendChar(module.char);
+
+    //         // Wait for 2 seconds
+    //         setTimeout(async () => {
+    //             if (isCancelled) return;
+
+    //             // Clear the character
+    //             await sendChar('.');
+
+    //             // Wait for 1 second, then repeat the cycle
+    //             setTimeout(() => {
+    //                 if (!isCancelled) displayAndClearChar();
+    //             }, 1000);
+    //         }, 2000);
+    //     };
+
+    //     displayAndClearChar(); // Start the sequence
+
+    //     // Cleanup function to stop the cycle
+    //     return () => {
+    //         isCancelled = true;
+    //     };
+    // }, [module.char]); // Dependency array ensures the character is up-to-date
+
+    const charToSend = module.char[module.char.indexOf('"') + 1];
+    sendChar(charToSend);
+
     return (
         <Box>
             <Typography variant='p'>
                 On this page, you will learn the character {module.char}, which
                 is represented with {module.representation}. Place your finger
-                over the braille display to feel the character appear and
-                reappear. When you’re ready to proceed, tap the “Next” button.
+                over the braille display to feel the character
+                {/* appear and
+                reappear*/}
+                . When you’re ready to proceed, tap the “Next” button.
             </Typography>
             {/* <button onClick={onComplete} disabled={isCompleted}>
             {isCompleted ? 'Completed' : 'Mark as Complete'}
