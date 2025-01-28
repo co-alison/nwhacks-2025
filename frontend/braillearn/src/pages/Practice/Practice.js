31264c99b970346832f305584812cbe6c387eb5c
alison03 <alison03@student.ubc.ca>
2025-01-28 12:26:35 -0800

    integrate speech recognition in modules

--- frontend/braillearn/src/pages/Practice/Practice.js
+++ frontend/braillearn/src/pages/Practice/Practice.js
@@ -7,6 +7,7 @@ import theme from '../../styles/theme';
 import { Box, Typography, Button, CircularProgress } from '@mui/material';
 import StyledButton from '../../components/StyledButton';
 import { sendChar } from '../../utils/serverApi';
+import { startListeningWithTimer } from '../../utils/speechRecognition';
 
 const Practice = () => {
     const [currentChar, setCurrentChar] = useState('');
@@ -33,7 +34,7 @@ const Practice = () => {
             });
         } else if (status === states.listen) {
             // setupAudio();
-            startListeningWithTimer();
+            startListeningWithTimer(timerRef, recognitionRef, setStatus, setCharInput, currentChar);
         } else if (status === states.correct) {
             speakText('Correct!');
         } else if (status === states.incorrect) {
@@ -51,35 +52,35 @@ const Practice = () => {
         };
     }, [status]);
 
-    const setupAudio = async () => {
-        try {
-            const audioContext = new window.AudioContext();
-            const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-            const microphone = audioContext.createMediaStreamSource(stream);
+    // const setupAudio = async () => {
+    //     try {
+    //         const audioContext = new window.AudioContext();
+    //         const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    //         const microphone = audioContext.createMediaStreamSource(stream);
 
-            const gainNode = audioContext.createGain();
-            gainNode.gain.value = 2;
+    //         const gainNode = audioContext.createGain();
+    //         gainNode.gain.value = 2;
 
-            const noiseSuppressionFilter = audioContext.createBiquadFilter();
-            noiseSuppressionFilter.type = 'lowpass';
-            noiseSuppressionFilter.frequency.value = 3000;
+    //         const noiseSuppressionFilter = audioContext.createBiquadFilter();
+    //         noiseSuppressionFilter.type = 'lowpass';
+    //         noiseSuppressionFilter.frequency.value = 3000;
 
-            // const analyser = audioContext.createAnalyser();
-            // analyser.fftSize = 256;
+    //         // const analyser = audioContext.createAnalyser();
+    //         // analyser.fftSize = 256;
 
-            microphone.connect(gainNode);
-            gainNode.connect(noiseSuppressionFilter);
-            // noiseSuppressionFilter.connect(analyser);
+    //         microphone.connect(gainNode);
+    //         gainNode.connect(noiseSuppressionFilter);
+    //         // noiseSuppressionFilter.connect(analyser);
 
-            audioContextRef.current = audioContext;
-            // analyserRef.current = analyser;
-            microphoneRef.current = microphone;
+    //         audioContextRef.current = audioContext;
+    //         // analyserRef.current = analyser;
+    //         microphoneRef.current = microphone;
 
-            // monitorVolume();
-        } catch (error) {
-            console.error("Error setting up audio:", error)
-        }
-    }
+    //         // monitorVolume();
+    //     } catch (error) {
+    //         console.error("Error setting up audio:", error)
+    //     }
+    // }
 
     // const monitorVolume = () => {
     //     const analyser = analyserRef.current;
@@ -94,146 +95,146 @@ const Practice = () => {
     //     volumeIntervalRef.current = setInterval(updateVolume, 100);
     // }
 
-    const stopAudio = () => {
-        if (microphoneRef.current) {
-            console.log('disconnect');
-            microphoneRef.current.disconnect();
-        }
-        if (audioContextRef.current) {
-            console.log("close");
-            audioContextRef.current.close();
-            audioContextRef.current = null;
-        }
+    // const stopAudio = () => {
+    //     if (microphoneRef.current) {
+    //         console.log('disconnect');
+    //         microphoneRef.current.disconnect();
+    //     }
+    //     if (audioContextRef.current) {
+    //         console.log("close");
+    //         audioContextRef.current.close();
+    //         audioContextRef.current = null;
+    //     }
 
-        if (volumeIntervalRef.current) {
-            clearInterval(volumeIntervalRef.current);
-        }
+    //     if (volumeIntervalRef.current) {
+    //         clearInterval(volumeIntervalRef.current);
+    //     }
 
-        setVolume(0);
-    }
+    //     setVolume(0);
+    // }
 
-    const startListeningWithTimer = () => {
-        console.log("start");
-        try {
-            const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
-            const speechGrammarList = new (window.SpeechGrammarList || window.webkitSpeechGrammarList)();
+    // const startListeningWithTimer = () => {
+    //     console.log("start");
+    //     try {
+    //         const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+    //         const speechGrammarList = new (window.SpeechGrammarList || window.webkitSpeechGrammarList)();
 
-            const grammar = `
-                #JSGF V1.0;
-                grammar letters;
-                public <letter> = A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q | R | S | T | U | V | W | X | Y | Z 
-                 | letter A | letter B | letter C | letter D | letter E | letter F | letter G | letter H | letter I 
-                 | letter J | letter K | letter L | letter M | letter N | letter O | letter P | letter Q | letter R 
-                 | letter S | letter T | letter U | letter V | letter W | letter X | letter Y | letter Z;
-                `;
-            speechGrammarList.addFromString(grammar, 1);
+    //         const grammar = `
+    //             #JSGF V1.0;
+    //             grammar letters;
+    //             public <letter> = A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q | R | S | T | U | V | W | X | Y | Z 
+    //              | letter A | letter B | letter C | letter D | letter E | letter F | letter G | letter H | letter I 
+    //              | letter J | letter K | letter L | letter M | letter N | letter O | letter P | letter Q | letter R 
+    //              | letter S | letter T | letter U | letter V | letter W | letter X | letter Y | letter Z;
+    //             `;
+    //         speechGrammarList.addFromString(grammar, 1);
 
-            recognition.grammars = speechGrammarList;
-            recognition.lang = 'en-US';
-            recognition.interimResults = false;
-            recognition.maxAlternatives = 1;
+    //         recognition.grammars = speechGrammarList;
+    //         recognition.lang = 'en-US';
+    //         recognition.interimResults = false;
+    //         recognition.maxAlternatives = 1;
 
-            recognition.onresult = (event) => {
-                clearTimeout(timerRef.current);
-                const spokenInput = event.results[0][0].transcript.trim().toLowerCase();
-                const confidence = event.results[0][0].confidence;
-                verifyChar(spokenInput, confidence);
-                recognition.stop();
-            }
+    //         recognition.onresult = (event) => {
+    //             clearTimeout(timerRef.current);
+    //             const spokenInput = event.results[0][0].transcript.trim().toLowerCase();
+    //             const confidence = event.results[0][0].confidence;
+    //             verifyChar(spokenInput, confidence);
+    //             recognition.stop();
+    //         }
 
-            recognition.onerror = (event) => {
-                console.error('Speech recognition error:', event.error);
+    //         recognition.onerror = (event) => {
+    //             console.error('Speech recognition error:', event.error);
 
-                if (event.error === "no-speech") {
-                    setStatus(states.noInput);
-                    setCharInput("No input received");
-                    return;
-                }
+    //             if (event.error === "no-speech") {
+    //                 setStatus(states.noInput);
+    //                 setCharInput("No input received");
+    //                 return;
+    //             }
 
-                clearTimeout(timerRef.current);
-                setStatus(states.retry);
-            };
+    //             clearTimeout(timerRef.current);
+    //             setStatus(states.retry);
+    //         };
 
-            recognition.onspeechend = () => {
-                console.log('speech end');
-                // recognition.abort();
-                // stopAudio();
-                // recognitionRef.current = null;
-            };
+    //         recognition.onspeechend = () => {
+    //             console.log('speech end');
+    //             // recognition.abort();
+    //             // stopAudio();
+    //             // recognitionRef.current = null;
+    //         };
 
-            recognition.onend = () => {
-                console.log("stopped");
-                recognition.abort();
-                recognitionRef.current = null;
-                // stopAudio();
-            }
+    //         recognition.onend = () => {
+    //             console.log("stopped");
+    //             recognition.abort();
+    //             recognitionRef.current = null;
+    //             // stopAudio();
+    //         }
 
-            recognition.start();
-            recognitionRef.current = recognition;
+    //         recognition.start();
+    //         recognitionRef.current = recognition;
 
-            timerRef.current = setTimeout(() => {
-                stopListeningDueToTimeout();
-            }, 10000);
-        } catch (error) {
-            console.error('Error starting speech recognition:', error);
-        }
-    }
+    //         timerRef.current = setTimeout(() => {
+    //             stopListeningDueToTimeout();
+    //         }, 10000);
+    //     } catch (error) {
+    //         console.error('Error starting speech recognition:', error);
+    //     }
+    // }
 
-    const stopListeningDueToTimeout = () => {
-        if (recognitionRef.current) {
-            recognitionRef.current.stop();
-            // stopAudio();
-            setStatus(states.noInput);
-            setCharInput("No input received");
-        }
-    }
+    // const stopListeningDueToTimeout = () => {
+    //     if (recognitionRef.current) {
+    //         recognitionRef.current.stop();
+    //         // stopAudio();
+    //         setStatus(states.noInput);
+    //         setCharInput("No input received");
+    //     }
+    // }
 
     const getRandomChar = () => {
         const index = Math.floor(Math.random() * characters.length);
         return characters[index];
     };
 
-    const verifyChar = async (input, confidence) => {
-        console.log('current', currentChar);
-        console.log('input', input);
-        console.log('confidence', confidence);
+    // const verifyChar = async (input, confidence) => {
+    //     console.log('current', currentChar);
+    //     console.log('input', input);
+    //     console.log('confidence', confidence);
 
-        if (input.trim().length === 1) {
-            if (input.trim().toLowerCase() === currentChar) {
-                setStatus(states.correct);
-                setCharInput(currentChar);
-                return;
-            } else {
-                setStatus(states.incorrect);
-                setCharInput(input.trim().toLowerCase());
-                return;
-            }
-        }
+    //     if (input.trim().length === 1) {
+    //         if (input.trim().toLowerCase() === currentChar) {
+    //             setStatus(states.correct);
+    //             setCharInput(currentChar);
+    //             return;
+    //         } else {
+    //             setStatus(states.incorrect);
+    //             setCharInput(input.trim().toLowerCase());
+    //             return;
+    //         }
+    //     }
 
-        if (input.startsWith("letter ") && input.length > 7 && confidence >= 0.5) {
-            const detectedLetter = input.split(" ")[1];
-            console.log(`detectedLetter: ${detectedLetter}`);
-            setCharInput(detectedLetter.toLowerCase());
+    //     if (input.startsWith("letter ") && input.length > 7 && confidence >= 0.5) {
+    //         const detectedLetter = input.split(" ")[1];
+    //         console.log(`detectedLetter: ${detectedLetter}`);
+    //         setCharInput(detectedLetter.toLowerCase());
 
-            if (detectedLetter.trim().toLowerCase() === currentChar.trim().toLowerCase()) {
-                setStatus(states.correct);
-            } else {
-                setStatus(states.incorrect);
-            }
-        } else if (confidence < 0.5 || !input.startsWith("letter")) {
-            // TODO: further process the input using NLP, currently asks user to try again on low confidence
-            // const splitInput = input.split(" ");
-            // for (const word of splitInput) {
-            //     const res = await axios.get(`http://localhost:3001/get-letter-nlp?input=${input}`);
+    //         if (detectedLetter.trim().toLowerCase() === currentChar.trim().toLowerCase()) {
+    //             setStatus(states.correct);
+    //         } else {
+    //             setStatus(states.incorrect);
+    //         }
+    //     } else if (confidence < 0.5 || !input.startsWith("letter")) {
+    //         // TODO: further process the input using NLP, currently asks user to try again on low confidence
+    //         // const splitInput = input.split(" ");
+    //         // for (const word of splitInput) {
+    //         //     const res = await axios.get(`http://localhost:3001/get-letter-nlp?input=${input}`);
 
-            // }
-            console.log("low confidence");
-            setStatus(states.retry);
-        } else {
-            console.log("unrecognized input", input);
-            setStatus(states.retry);
-        }
-    }
+    //         // }
+    //         console.log("low confidence");
+    //         setStatus(states.retry);
+    //     } else {
+    //         console.log("unrecognized input", input);
+    //         setStatus(states.retry);
+    //     }
+    // }
 
     const reset = () => {
         // const clear = '.';
